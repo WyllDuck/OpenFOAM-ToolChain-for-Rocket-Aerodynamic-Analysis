0e3d55aff68e2375c694939a503e0635fdcb0313
Felix Marti <martivalverde.98@gmail.com>
2023-09-04 00:37:02 +0200

    rhoPimpleFoam Transonic solver p changed, notfound

--- templates/multi_template_run.sh
+++ templates/multi_template_run.sh
@@ -7,8 +7,8 @@ mkdir -p ../todo
 #./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R3_rhoCentralFoam.json rhoCentralFoam ../todo/Ma2.3_AoA8_R3_rhoCentralFoam
 #./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R4_rhoCentralFoam.json rhoCentralFoam ../todo/Ma2.3_AoA8_R4_rhoCentralFoam
 
-./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R0_rhoPimpleFoam.json rhoPimpleFoam ../todo/Ma2.3_AoA8_R0_rhoPimpleFoam
-./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R1_rhoPimpleFoam.json rhoPimpleFoam ../todo/Ma2.3_AoA8_R1_rhoPimpleFoam
-./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R2_rhoPimpleFoam.json rhoPimpleFoam ../todo/Ma2.3_AoA8_R2_rhoPimpleFoam
-./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R3_rhoPimpleFoam.json rhoPimpleFoam ../todo/Ma2.3_AoA8_R3_rhoPimpleFoam
-./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R4_rhoPimpleFoam.json rhoPimpleFoam ../todo/Ma2.3_AoA8_R4_rhoPimpleFoam
+./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R1_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R1_rhoPimpleFoam
+./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R2_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R2_rhoPimpleFoam
+./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R3_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R3_rhoPimpleFoam
+./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R4_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R4_rhoPimpleFoam
+./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R5_rhoPimpleFoam
