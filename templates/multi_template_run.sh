9589da4365313845e958f751d6838ff6a5dc6669
Felix Marti <martivalverde.98@gmail.com>
2023-09-07 11:52:13 +0200

    small changes MapFieldsPar and other

--- templates/multi_template_run.sh
+++ templates/multi_template_run.sh
@@ -7,8 +7,64 @@ mkdir -p ../todo
 #./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R3_rhoCentralFoam.json rhoCentralFoam ../todo/Ma2.3_AoA8_R3_rhoCentralFoam
 #./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R4_rhoCentralFoam.json rhoCentralFoam ../todo/Ma2.3_AoA8_R4_rhoCentralFoam
 
-./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R1_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R1_rhoPimpleFoam
-./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R2_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R2_rhoPimpleFoam
-./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R3_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R3_rhoPimpleFoam
-./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R4_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R4_rhoPimpleFoam
-./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R5_rhoPimpleFoam
+# ./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R1_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R1_rhoPimpleFoam
+# ./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R2_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R2_rhoPimpleFoam
+# ./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R3_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R3_rhoPimpleFoam
+# ./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R4_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R4_rhoPimpleFoam
+# ./template_run.sh /mnt/e/openfoam-data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA8_R5_rhoPimpleFoam
+
+
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA0_R1_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.0_AoA0_R1_rhoPimpleFoam
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA0_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.0_AoA0_R5_rhoPimpleFoam
+
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA8_R1_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.0_AoA8_R1_rhoPimpleFoam
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA8_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.0_AoA8_R5_rhoPimpleFoam
+
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA16_R1_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.0_AoA16_R1_rhoPimpleFoam
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA16_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.0_AoA16_R5_rhoPimpleFoam
+
+
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA0_R1_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.5_AoA0_R1_rhoPimpleFoam
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA0_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.5_AoA0_R5_rhoPimpleFoam
+
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA8_R1_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.5_AoA8_R1_rhoPimpleFoam
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA8_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.5_AoA8_R5_rhoPimpleFoam
+
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA16_R1_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.5_AoA16_R1_rhoPimpleFoam
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA16_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma1.5_AoA16_R5_rhoPimpleFoam
+
+# # Test initialization speed
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma2.3_AoA0_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA0_R5_rhoPimpleFoam
+
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma2.3_AoA16_R1_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA16_R1_rhoPimpleFoam
+# ./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma2.3_AoA16_R5_rhoPimpleFoam.json rhoPimpleFoamTransonic ../todo/Ma2.3_AoA16_R5_rhoPimpleFoam
+
+
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA0_R1_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.0_AoA0_R1_rhoCentralFoam
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA0_R5_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.0_AoA0_R5_rhoCentralFoam
+
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA8_R1_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.0_AoA8_R1_rhoCentralFoam
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA8_R5_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.0_AoA8_R5_rhoCentralFoam
+
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA16_R1_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.0_AoA16_R1_rhoCentralFoam
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.0_AoA16_R5_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.0_AoA16_R5_rhoCentralFoam
+
+
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA0_R1_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.5_AoA0_R1_rhoCentralFoam
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA0_R5_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.5_AoA0_R5_rhoCentralFoam
+
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA8_R1_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.5_AoA8_R1_rhoCentralFoam
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA8_R5_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.5_AoA8_R5_rhoCentralFoam
+
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA16_R1_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.5_AoA16_R1_rhoCentralFoam
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma1.5_AoA16_R5_rhoCentralFoam.json rhoCentralFoam ../todo/Ma1.5_AoA16_R5_rhoCentralFoam
+
+
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma2.3_AoA0_R1_rhoCentralFoam.json rhoCentralFoam ../todo/Ma2.3_AoA0_R1_rhoCentralFoam
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma2.3_AoA0_R5_rhoCentralFoam.json rhoCentralFoam ../todo/Ma2.3_AoA0_R5_rhoCentralFoam
+
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R1_rhoCentralFoam.json rhoCentralFoam ../todo/Ma2.3_AoA8_R1_rhoCentralFoam
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R5_rhoCentralFoam.json rhoCentralFoam ../todo/Ma2.3_AoA8_R5_rhoCentralFoam
+
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma2.3_AoA16_R1_rhoCentralFoam.json rhoCentralFoam ../todo/Ma2.3_AoA16_R1_rhoCentralFoam
+./template_run.sh ../../WARR_thermal_simulation/configs/config_Ma2.3_AoA16_R5_rhoCentralFoam.json rhoCentralFoam ../todo/Ma2.3_AoA16_R5_rhoCentralFoam
