44762e79a9a2a91c6ebc86c7cce102f4ae287b96
Félix Martí Valverde <ge95muk@mytum.de>
2023-10-12 19:09:54 +0200

    rhoCentral subsonic

--- templates/multi_template_run.sh
+++ templates/multi_template_run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 mkdir -p ../todo
 
-./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma2.3_AoA8_R1_rhoCentralFoam.json rhoCentralFoam /data/sphereCases/todo/Ma2.3_AoA8_R1_rhoCentralFoam_WSL2
+
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma0.6_AoA0_R1_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma0.6_AoA0_R1_rhoCentralFoam
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma0.6_AoA8_R1_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma0.6_AoA8_R1_rhoCentralFoam
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma0.6_AoA16_R1_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma0.6_AoA16_R1_rhoCentralFoam
+
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma0.6_AoA0_R5_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma0.6_AoA0_R5_rhoCentralFoam
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma0.6_AoA8_R5_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma0.6_AoA8_R5_rhoCentralFoam
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma0.6_AoA16_R5_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma0.6_AoA16_R5_rhoCentralFoam
+
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma1.0_AoA0_R1_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma1.0_AoA0_R1_rhoCentralFoam
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma1.0_AoA8_R1_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma1.0_AoA8_R1_rhoCentralFoam
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma1.0_AoA16_R1_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma1.0_AoA16_R1_rhoCentralFoam
+
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma1.0_AoA0_R5_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma1.0_AoA0_R5_rhoCentralFoam
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma1.0_AoA8_R5_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma1.0_AoA8_R5_rhoCentralFoam
+./template_run.sh /data/WARR_thermal_simulation/configs/config_Ma1.0_AoA16_R5_rhoCentralFoam.json rhoCentralFoamSubsonic /data/sphereCases/todo/Ma1.0_AoA16_R5_rhoCentralFoam
