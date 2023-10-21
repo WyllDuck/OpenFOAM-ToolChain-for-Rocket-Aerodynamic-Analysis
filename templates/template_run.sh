5c61845c1f03ea26a0532c32c040cc2d25545679
Félix Martí Valverde <ge95muk@mytum.de>
2023-10-21 17:01:08 +0200

    changes to plot system, improvement + pimple fvSol

--- templates/template_run.sh
+++ templates/template_run.sh
@@ -4,26 +4,32 @@ CONFIG=$1                   # passing configuration file - contains case values
 PROJECT_FOLDER=$2           # address of template folder
 OUTPUT_PROJECT_FOLDER=$3    # output folder for the resulting templated project
 
+echo ""
 echo "Configuration File: " $CONFIG
 echo "Project Template Folder: " $PROJECT_FOLDER
 echo "Project Output Folder: " $OUTPUT_PROJECT_FOLDER
 
-# if OUTPUT_PROJECT_FOLDER does already exist stop
+# if OUTPUT_PROJECT_FOLDER does already ask if overwrite
 if [ -d "$OUTPUT_PROJECT_FOLDER" ]; then
-    echo "Error: Project Output Folder already exists!"
+    echo "Project Output Folder already exists!"
+    read -p "Do you want to overwrite it? (y/n) " -n 1 -r
     echo ""
-    exit 1
+
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Aborted!"
+        exit 1
+    fi
+    # else remove OUTPUT_PROJECT_FOLDER
+    rm -rf $OUTPUT_PROJECT_FOLDER
 fi
 
-# copy template in 'run' folder and change directory
 # if PROJECT_FOLDER does not exist stop
 if [ ! -d "$PROJECT_FOLDER" ]; then
     echo "Error: Project Template Folder does not exist!"
     echo ""
     exit 1
 fi
-
-cp -r $PROJECT_FOLDER $OUTPUT_PROJECT_FOLDER
 
 # replace default configuration file in template repository with the used configuration file
 # if CONFIG does not exist stop
@@ -32,6 +38,8 @@ if [ ! -f "$CONFIG" ]; then
     echo ""
     exit 1
 fi
+
+cp -r $PROJECT_FOLDER $OUTPUT_PROJECT_FOLDER
 cp $CONFIG $OUTPUT_PROJECT_FOLDER/config.json
 
 # find all 'jinja' templates and set fixed values from the configuration file 
